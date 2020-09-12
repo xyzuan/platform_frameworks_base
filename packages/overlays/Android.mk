1b5171d9cac8ccc5102c889fb06d998c67a4d763
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-12 12:10:28 +0530

    overlays: Add Pixel Blue accent
     * from sunfish 11.0 release
    
    Change-Id: Id66210e8dcf021dc41f181ccac119d397d794504

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -24,6 +24,7 @@ LOCAL_REQUIRED_MODULES := \
 	AccentColorSpaceOverlay \
 	AccentColorGreenOverlay \
 	AccentColorPurpleOverlay \
+	AccentColorPixelBlueOverlay \
 	DisplayCutoutEmulationCornerOverlay \
 	DisplayCutoutEmulationDoubleOverlay \
         DisplayCutoutEmulationHoleOverlay \
